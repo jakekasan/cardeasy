5e70ff78270fe3b45a8d2d042020d1a55eea34cc
macmini <jake.kasan@gmail.com>
2019-08-23 20:03:11 +0100

    Add tests for Card controller

---
+++ test/card.controller.test.js
@@ -0,0 +1,39 @@
+var assert = require("assert");
+
+
+describe("Card Controller", () => {
+    
+    describe("newCard", () => {
+
+        it("Should exist", () => {
+            assert(true);
+        });
+
+        it("Should set a cookie", () => {
+            assert(true);
+        });
+
+    });
+
+    describe("newCard_POST", () => {
+
+        it("Should exist", () => {
+            assert(true);
+        });
+
+        it("Should ", () => {
+            assert(true);
+        })
+    });
+
+    describe("signCard", () => {
+
+        it("Should exist", () => {
+            assert(true);
+        });
+
+        it("Something", () => {
+            assert(true);
+        });
+    });
+})
