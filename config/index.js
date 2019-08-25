aa107c6fd879cb5c7e5d9f2d57717d4fc7929254
macmini <jake.kasan@gmail.com>
2019-08-25 15:24:03 +0100

    Add Config
    Added index file with development config.

---
+++ config/index.js
@@ -0,0 +1,32 @@
+/*
+
+
+*/
+
+let config = {
+    "development":{
+        port:8080,
+        address:"localhost",
+        db: {
+            Sequelize: {
+                host: "localhost",
+                dialect: "sqlite",
+                pool: {
+                    max: 5,
+                    min: 0,
+                    idle: 10000
+                },
+                storage: "./data.db"
+            },
+            Mongoose: {
+
+            }
+        }
+    }
+}
+
+
+
+module.exports = (mode) => {
+    return config[mode] || config.development
+}
