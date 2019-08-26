3fb5bd664fb84391190052c3d4f55e3d4e2490ae
macmini <jake.kasan@gmail.com>
2019-08-26 13:28:02 +0100

    Update User Session middleware
    If a session ID is present in a cookie, the sessionData is added.

--- middleware/userSession.middleware.js
+++ middleware/userSession.middleware.js
@@ -4,18 +4,29 @@ Loads usersession (if any) into the
 
 */
 
-const User = require("./../models/user.model");
+const Session = require("./../models/session.model");
 
 module.exports = (req, res, next) => {
     /*
 
+        If a valid sessionID is in the cookie, add session data to the request object.
 
     */
 
     // get cookie
-    if (req.cookie) {
-        let {id} = req.cookie;
+    if (req.cookies && req.cookies.id) {
+        Session.get({sessionID: id})
+            .then((sessionData) => {
+                req._session = sessionData;
+
+                console.log("Session data:", sessionData);
+                next();
+            })
+            .catch(() => {
+                next();
+            })
+
     } else {
-        User.new
+        next();
     }
 }
