9ba34d2a0886b9da48f3b53b4f48cc174d1d9025
jakekasan <jake.kasan@gmail.com>
2019-10-08 23:17:15 +0100

    Add LoginFailed
    Empty react component for now...

---
+++ src/react/components/signExistingCard/loginFailed.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class LoginFailed extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <h1>Login Failed!</h1>
+        )
+    }
+}
+
+export default LoginFailed;
