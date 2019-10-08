00c34b4222e196112a0e93b3169b54752c49551b
jakekasan <jake.kasan@gmail.com>
2019-10-08 23:14:51 +0100

    Update SignExistingCard component
    Added callback for handling a correct login from child Login component

--- src/react/components/signExistingCard/main.jsx
+++ src/react/components/signExistingCard/main.jsx
@@ -6,16 +6,25 @@ import ChooseCard from "./chooseCard.jsx";
 class SignExistingCard extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            userData: null
+        };
     }
 
     componentDidMount() {
         console.log("SignExistingCard mounted");
     }
 
+    handleCorrectLogin(userData) {
+        this.setState(() => {
+            return {userData: userData}
+        })
+    }
+
     render() {
         return (
             <div>
-                { (this.props.userData) ? <ChooseCard userData = {this.props.userData} /> : <Login /> }
+                { (this.state && this.state.userData) ? <ChooseCard userData = {this.state.userData} /> : <Login onCorrectLogin = { (userData) => handleCorrectLogin(userData) }/> }
             </div>
         )
     }
