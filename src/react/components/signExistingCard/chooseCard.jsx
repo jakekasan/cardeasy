f1a20a4d87f34a653e79f78801f0cb72ee17ba0d
jakekasan <jake.kasan@gmail.com>
2019-10-08 23:16:35 +0100

    Add ChoseCard component
    Added empty react component

---
+++ src/react/components/signExistingCard/chooseCard.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class ChooseCard extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <h1>ChooseCard</h1>
+        )
+    }
+}
+
+export default ChooseCard;
