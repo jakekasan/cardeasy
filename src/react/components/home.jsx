8e45f8bc7c6a62d74a9e18bd51dd8586ccd7b62c
jakekasan <jake.kasan@gmail.com>
2019-10-08 23:15:12 +0100

    Add Home component
    Empty React component for now...

---
+++ src/react/components/home.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <header>
+                <h1>This is home!</h1>
+            </header>
+        )
+    }
+}
+
+export default Home;
