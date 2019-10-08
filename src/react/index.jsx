e0a8d5a0ac886114e27c8cb153c6e6cdc97dafd6
jakekasan <jake.kasan@gmail.com>
2019-10-08 23:14:04 +0100

    Update App component
    Fixed routes so that specific components are rendered instead of placeholders

--- src/react/index.jsx
+++ src/react/index.jsx
@@ -3,18 +3,16 @@ import ReactDOM from "react-dom";
 import { Route, BrowserRouter as Router } from "react-router-dom";
 import Header from "./components/header.jsx";
 import Footer from "./components/footer.jsx";
+import SignExistingCard from "./components/signExistingCard/main.jsx";
+import Home from "./components/home.jsx";
 
 const App = () => {
     return (
         <section>
             <Header />
             <Router>
-                <Route exact path="/" component= { () => {
-                    <h1>Cahooonah</h1>
-                } } />
-                <Route path="/sign" component = { () => {
-                    <h1> Not Cahoonah</h1>
-                } }/>
+                <Route exact path="/" component= {Home} />
+                <Route path="/sign" component = {SignExistingCard}/>
             </Router>
             <Footer />
         </section>
