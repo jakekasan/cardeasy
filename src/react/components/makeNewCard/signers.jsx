01b461fdb41cee15210ccacde63942d0767e7bda
jakekasan <jake.kasan@gmail.com>
2019-10-09 20:28:06 +0100

    Update Signers
    Added empty react class

---
+++ src/react/components/makeNewCard/signers.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Signers extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <h1>Signers</h1>
+        )
+    }
+}
+
+export default Signers;
