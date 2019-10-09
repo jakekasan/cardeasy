778077219ea93923d5e29a065a7f136b80a91edd
jakekasan <jake.kasan@gmail.com>
2019-10-09 22:17:29 +0100

    Update MakeNewCard component
    Added react class

---
+++ src/react/components/makeNewCard/main.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class MakeNewCard extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <h1>Make New Card</h1>
+        )
+    }
+}
+
+export default MakeNewCard;
