68d3c1e75a095f8b30ec8eae5657097a4f4392a2
jakekasan <jake.kasan@gmail.com>
2019-10-09 21:17:27 +0100

    Update App component
    Add /new route to Router and Nav

--- src/react/index.jsx
+++ src/react/index.jsx
@@ -4,15 +4,19 @@ import { Route, BrowserRouter as Router } from "react-router-dom";
 import Header from "./components/header.jsx";
 import Footer from "./components/footer.jsx";
 import SignExistingCard from "./components/signExistingCard/main.jsx";
+import MakeNewCard from "./components/makeNewCard/main.jsx";
 import Home from "./components/home.jsx";
+import Nav from "./components/nav.jsx";
 
 const App = () => {
     return (
         <section>
             <Header />
+            <Nav />
             <Router>
-                <Route exact path="/" component= {Home} />
-                <Route path="/sign" component = {SignExistingCard}/>
+                <Route exact path="/" component= { Home } />
+                <Route path="/sign" component = { SignExistingCard }/>
+                <Route path="/new" component = { MakeNewCard } />
             </Router>
             <Footer />
         </section>
