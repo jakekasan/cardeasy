077d3b302604097328db103ac39a7588c1c06108
jakekasan <jake.kasan@gmail.com>
2019-10-09 20:23:54 +0100

    Update Content component
    Added empty react class

---
+++ src/react/components/makeNewCard/content.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Content extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <h1>Content</h1>
+        )
+    }
+}
+
+export default Content;
