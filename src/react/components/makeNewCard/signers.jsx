f5c5010733958572fd28e0743173521626f4db77
jakekasan <jake.kasan@gmail.com>
2019-10-10 01:02:12 +0100

    Update Signers component
    Added helper functions for handling dynamic list of signers.

--- src/react/components/makeNewCard/signers.jsx
+++ src/react/components/makeNewCard/signers.jsx
@@ -3,11 +3,59 @@ import React, { Component } from "react";
 class Signers extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            recipients: {}
+        }
+    }
+
+    getNewRecipients(){
+        let recipientKeys = Object.keys(this.state.recipients);
+
+        let recipientArray = recipientKeys.map(item => {
+            return { id:item, ...this.state.recipients[item] }
+        });
+
+        let activeInputFields = this.state.reduce((acc, cur) => {
+            let { name, email } = cur;
+            if (name && name == "" && email && email == "") {
+                return acc + 1
+            } else {
+                return acc
+            }
+        }, 0);
+
+        return activeInputFields
+    }
+
+    onUpdateRecipientField(e) {
+        e.persist();
+        let id = e.target.dataset.id;
+        this.setState((state) => {
+            let targetId = state.recipient[id];
+
+            targetId[e.target.name] = e.target.value;
+
+            state.recipient[id] = targetId;
+            return state
+        });
+    }
+
+    makeInputsLi(id){
+        return (
+            <li>
+                <input type="text" name="name" id="name" data-id={id} onChange = {this.onUpdateRecipientField} />
+                <input type="email" name="email" id="email" data-id={id} onChange = {this.onUpdateRecipientField}/>
+            </li>
+        )
     }
 
     render() {
+        
+
         return (
-            <h1>Signers</h1>
+            <ul>
+                { recipients }
+            </ul>
         )
     }
 }
