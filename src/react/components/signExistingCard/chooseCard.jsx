ee3dd0ca941d38db1f2697346358dce1c9e92940
jakekasan <jake.kasan@gmail.com>
2019-11-04 21:09:18 +0000

    Update ChooseCard component
    Added state logic for selecting and signing cards

--- src/react/components/signExistingCard/chooseCard.jsx
+++ src/react/components/signExistingCard/chooseCard.jsx
@@ -1,13 +1,39 @@
 import React, { Component } from "react";
 
+import Card from "./card.jsx";
+import CardList from "./cardList.jsx";
+
 class ChooseCard extends Component {
     constructor(props) {
         super(props);
+        this.state = {};
+    }
+
+    componentDidMount() {
+        this.setState(() => {
+
+        })
+    }
+
+    handleSelectCard(e) {
+        e.persist();
+        console.log(e.target.dataset.cardNumber);
+        this.setState((currentState) => {
+            return {...currentState, selectedCard: e.target.dataset.cardNumber }
+        });
+    }
+
+    handleDeselectCard(e) {
+        this.setState((currentState) => {
+            return {...currentState, selectedCard: null}
+        });
     }
 
     render() {
         return (
-            <h1>ChooseCard</h1>
+            <section>
+                ({ (this.state && this.state.selectCard) }) ? <Card /> : <CardList />
+            </section>
         )
     }
 }
