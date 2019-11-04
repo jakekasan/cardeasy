bc6e107dbfe72d41c9140d7dfc479f96c3b4a3c3
jakekasan <jake.kasan@gmail.com>
2019-11-04 20:34:24 +0000

    Update Signers
    Fixed name and added logic for dynamic list of signers.

--- src/react/components/makeNewCard/signers.jsx
+++ src/react/components/makeNewCard/signers.jsx
@@ -4,65 +4,95 @@ class Signers extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            recipients: {}
+            signers: [
+                {
+                    name: "Jake",
+                    email: "me@mail.com"
+                },
+                {
+                    name: "Carla",
+                    email: "carla@mail.com"
+                }
+            ]
         }
 
         this.activeInputFields = this.activeInputFields.bind(this);
-        this.onUpdateRecipientField = this.onUpdateRecipientField.bind(this);
+        this.onUpdateSignerField = this.onUpdateSignerField.bind(this);
     }
 
     activeInputFields(){
-        let recipientKeys = Object.keys(this.state.recipients);
+        let signerKeys = Object.keys(this.state.signers);
 
-        let recipientArray = recipientKeys.map(item => {
-            return { id:item, ...this.state.recipients[item] }
+        let signerArray = signerKeys.map(item => {
+            return { id:item, ...this.state.signers[item] }
         });
 
-        let activeInputFields = this.state.reduce((acc, cur) => {
+        let activeInputFields = this.state.signers.reduce((acc, cur) => {
             let { name, email } = cur;
-            if (name && name == "" && email && email == "") {
+            if (name && name != "" && email && email != "") {
                 return acc + 1
             } else {
                 return acc
             }
         }, 0);
 
+        console.log(activeInputFields);
+
         return activeInputFields
     }
 
-    onUpdateRecipientField(e) {
+    onUpdateSignerField(e) {
         e.persist();
         let id = e.target.dataset.id;
+        let name = e.target.name;
+
+        let newData = {};
+        newData[name] = e.target.value;
+
         this.setState((state) => {
-            let targetId = state.recipient[id];
+            let oldData = state.signers[id] || {};
 
-            targetId[e.target.name] = e.target.value;
+            let reconciled = {...oldData, ...newData};
+            if (reconciled.name != "" || reconciled.email != "") {
+                state.signers[id] = reconciled;
+            } else {
+                state.signers.pop(id);
+            }
 
-            state.recipient[id] = targetId;
+            console.log(state);
+            
             return state
         });
     }
 
     makeInputsLi({id, name, email}){
+        console.log(`Rendering id: ${id}, name: ${name}, email: ${email}`);
         return (
-            <li>
-                <input type="text" name="name" id="name" data-id={id} onChange = {this.onUpdateRecipientField} />
-                <input type="email" name="email" id="email" data-id={id} onChange = {this.onUpdateRecipientField}/>
+            <li key={id}>
+                <input type="text" name="name" id="name" data-id={id} value = {name} onChange = {this.onUpdateSignerField} />
+                <input type="email" name="email" id="email" data-id={id} value = {email} onChange = {this.onUpdateSignerField}/>
             </li>
         )
     }
 
     render() {
-        let recipients = (Object.keys())
-            .map(item => {
-                return {id: item, ...this.state.recipients[item]}
+        let signers = this.state.signers
+            .map((item, id) => {
+                return {id: id, ...item}
             })
             .map(item => {
-                return this.makeInputsLi()
+                return this.makeInputsLi(item)
             })
+
+        signers.push(this.makeInputsLi({
+            id: this.activeInputFields() + 1,
+            name: "",
+            email: ""
+        }))
+
         return (
             <ul>
-                { recipients }
+                { signers }
             </ul>
         )
     }
