3793274f31e5061ed25fd82e8321c682e57b705c
jakekasan <jake.kasan@gmail.com>
2019-11-04 21:06:43 +0000

    Add CardList component
    New component for showing a list of cards a person can sign.

---
+++ src/react/components/signExistingCard/cardList.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class CardList extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <h1>CardList</h1>
+        )
+    }
+}
+
+export default CardList;
