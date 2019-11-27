4369301283c2e2262a7e07b40c2ca46a3e3de81c
jakekasan <jake.kasan@gmail.com>
2019-11-27 14:02:37 +0000

    Update MakeNewCard component
    Added placeholder submit logic and handleChange to update form data

--- src/react/components/makeNewCard/main.jsx
+++ src/react/components/makeNewCard/main.jsx
@@ -3,19 +3,45 @@ import React, { Component } from "react";
 import Recipient from "./recipient.jsx";
 import Content from "./content.jsx";
 import Signers from "./signers.jsx";
+import SubmitButton from "./submitButton.jsx";
 
 class MakeNewCard extends Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            formData: {}
+        }
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleSubmit(e) {
+        e.persist();
+
+        e.preventDefault();
+
+        console.log(this.state);
+        // TODO: Should call out to API
+        return
+    }
+
+    handleChange(callBack) {
+        this.setState((state) => {
+            state.formData = callBack(state.formData);
+            return state
+        });
     }
 
     render() {
         return (
-            <section id="newCard">
-                <Recipient />
-                <Content />
-                <Signers />
-            </section>
+            <form id="newCard" onSubmit = {this.handleSubmit}>
+                <Recipient onChange = { this.handleChange } />
+                <Content onChange = { this.handleChange } />
+                <Signers onChange = { this.handleChange } />
+                <button type="submit" >Send your card!</button>
+            </form>
         )
     }
 }
