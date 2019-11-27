a621639a5d3b391e4838bf9f78ca71b4a3279295
jakekasan <jake.kasan@gmail.com>
2019-11-27 14:03:10 +0000

    Update Recipient component
    Added event handler for when input value is changed

--- src/react/components/makeNewCard/recipient.jsx
+++ src/react/components/makeNewCard/recipient.jsx
@@ -4,15 +4,31 @@ import { Link } from "react-router-dom";
 class Recipient extends Component {
     constructor(props) {
         super(props);
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        event.persist();
+
+        let fieldName = event.target.name;
+        let fieldValue = event.target.value;
+
+        this.props.onChange((formData) => {
+            let update = {};
+            update[fieldName] = fieldValue;
+            formData.recipient = {...formData.recipient, ...update };
+            return formData
+        })
     }
 
     render() {
         return (
             <section id="recipientSection">
                 <label htmlFor="recipientName">Name of recipient:</label>
-                <input type="text" name="recipientName" id="recipientName"/>
+                <input type="text" name="recipientName" id="recipientName" onChange = { this.handleChange } />
                 <label htmlFor="recipientEmail">Name of recipient:</label>
-                <input type="email" name="recipientEmail" id="recipientEmail"/>
+                <input type="email" name="recipientEmail" id="recipientEmail" onChange = { this.handleChange } />
             </section>
         )
     }
