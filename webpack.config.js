ea4d65f6713066b47650a222cab221c7b9a7cb51
jakekasan <jake.kasan@gmail.com>
2020-01-12 18:47:53 +0000

    Add SASS loader to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,10 @@ module.exports = {
                 }
             },
             {
+                test: /\.(scss|sass|css)/,
+                use: [ "style-loader", "css-loader", "sass-loader" ]
+            },
+            {
                 test: /\.html$/,
                 use: [
                     {
