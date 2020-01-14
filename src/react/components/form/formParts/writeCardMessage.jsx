7c2bca04d26bad59c32c4991700970521ed44a04
jakekasan <jake.kasan@gmail.com>
2020-01-14 21:13:45 +0000

    Add WriteCardMessage component

---
+++ src/react/components/form/formParts/writeCardMessage.jsx
@@ -0,0 +1,11 @@
+export const WriteCardMessage = ({
+    values,
+    onChange
+}) => {
+    return (
+        <section>
+            <h5>Step {stepNumber}: Write your message</h5>
+            <textarea></textarea>
+        </section>
+    )
+}
