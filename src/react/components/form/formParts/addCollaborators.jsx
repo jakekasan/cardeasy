cbd29e03fce08c8af65cc52cf118c30e72e60143
jakekasan <jake.kasan@gmail.com>
2020-01-14 21:12:44 +0000

    Add AddCollaborators component

---
+++ src/react/components/form/formParts/addCollaborators.jsx
@@ -0,0 +1,15 @@
+export const AddCollaborators = ({
+    values,
+    onChange
+}) => {
+    return (
+        <section>
+            <h5>
+                Step {stepNumber}: Add card collaborators
+            </h5>
+            <ul>
+                <li><p>Collaborator</p></li>
+            </ul>
+        </section>
+    )
+}
