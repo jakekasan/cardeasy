e1a2701a1ee6e1cabcf697b1836c36748a2c3e88
jakekasan <jake.kasan@gmail.com>
2020-01-19 22:23:56 +0000

    Update Collaborators component
    Collaborators now result of map instead of standalone component.

--- src/react/components/form/formParts/collaborators/collaborators.jsx
+++ src/react/components/form/formParts/collaborators/collaborators.jsx
@@ -7,39 +7,105 @@ export const Collaborators = ({
     values,
     stepNumber
 }) => {
+
+    function collaboratorOnChange(event){
+        let target = event.target;
+        let parentNode = ((target.parentNode).parentNode);
+
+        parentNode.children.map(child => {
+            console.log()
+        })
+    }
+
+    const collaborators = values.collaborators.map((collaborator, i) => {
+        return <CollaboratorDetails
+            name = {{
+                elementName: "collaboratorName",
+                elementLabel: "Collaborator's Name",
+                elementValue: collaborator.name
+            }}
+            email = {{
+                elementName: "collaboratorEmail",
+                elementLabel: "Collaborator's Email",
+                elementValue: collaborator.email
+            }}
+            onChange = { collaboratorOnChange }
+            type = "collaborator"
+            key = { i + 2 }
+        />
+    })
+
     return (
         <section>
             <h5>Step {stepNumber}: Recipient Details</h5>
-            <CollaboratorDetails
-                name = {{
-                    elementName: "recipientName",
-                    elementLabel: "Recipient's Name",
-                    value: values.recipientName
-                }}
-                email = {{
-                    elementName: "recipientEmail",
-                    elementLabel: "Recipient's Email",
-                    value: values.recipientEmail
-                }}
-                onChange = { onChange } />
+            <ul>
+                <CollaboratorDetails
+                    name = {{
+                        elementName: "recipientName",
+                        elementLabel: "Recipient's Name",
+                        value: values.recipientName
+                    }}
+                    email = {{
+                        elementName: "recipientEmail",
+                        elementLabel: "Recipient's Email",
+                        value: values.recipientEmail
+                    }}
+                    onChange = { onChange }
+                    key = {0}/>
 
-            <CollaboratorDetails
-                name = {{
-                    elementName: "recipientName",
-                    elementLabel: "Recipient's Name",
-                    value: values.recipientName
-                }}
-                email = {{
-                    elementName: "recipientEmail",
-                    elementLabel: "Recipient's Email",
-                    value: values.recipientEmail
-                }}
-                onChange = { onChange } />
+                <CollaboratorDetails
+                    name = {{
+                        elementName: "senderName",
+                        elementLabel: "Sender's Name",
+                        value: values.senderName
+                    }}
+                    email = {{
+                        elementName: "senderEmail",
+                        elementLabel: "Senser's Email",
+                        value: values.senderEmail
+                    }}
+                    onChange = { onChange }
+                    key = {1} />
 
-            <CollaboratorList
-                masterOnChange = {onChange}
-                collaborators = { values.collaborators }/>
+                { collaborators }
 
+            </ul>
+            
         </section>
     )
+
+    // return (
+    //     <section>
+    //         <h5>Step {stepNumber}: Recipient Details</h5>
+            
+    //         <CollaboratorDetails
+    //             name = {{
+    //                 elementName: "recipientName",
+    //                 elementLabel: "Recipient's Name",
+    //                 value: values.recipientName
+    //             }}
+    //             email = {{
+    //                 elementName: "recipientEmail",
+    //                 elementLabel: "Recipient's Email",
+    //                 value: values.recipientEmail
+    //             }}
+    //             onChange = { onChange } />
+
+    //         <CollaboratorDetails
+    //             name = {{
+    //                 elementName: "recipientName",
+    //                 elementLabel: "Recipient's Name",
+    //                 value: values.recipientName
+    //             }}
+    //             email = {{
+    //                 elementName: "recipientEmail",
+    //                 elementLabel: "Recipient's Email",
+    //                 value: values.recipientEmail
+    //             }}
+    //             onChange = { onChange } />
+
+    //         { collaborators }
+
+    //     </section>
+    // )
 }
