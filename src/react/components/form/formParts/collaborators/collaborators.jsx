879c306f323ce16368e941a3b0d0d0687239632e
jakekasan <jake.kasan@gmail.com>
2020-02-10 11:50:14 +0000

    Update old Collaborators component
    Forgot to commit changes to old Collabs component. Redundant now.

--- src/react/components/form/formParts/collaborators/collaborators.jsx
+++ src/react/components/form/formParts/collaborators/collaborators.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { CollaboratorDetails } from "./collaboratorDetails.jsx";
-import { CollaboratorList } from "./collaboratorList.jsx";
+import { CollaboratorList } from "./../collaboratorList.jsx";
 
 export const Collaborators = ({
     onChange,
@@ -12,9 +12,36 @@ export const Collaborators = ({
         let target = event.target;
         let parentNode = ((target.parentNode).parentNode);
 
-        parentNode.children.map(child => {
-            console.log()
+        const collaborators = Array.from(parentNode.childNodes)
+            .filter(child => child.getAttribute("data-type") === "collaborator")
+            .map(child => {
+                let collaboratorName = child.querySelector("input[name='collaboratorName'").value;
+                
+                let collaboratorEmail = child.querySelector("input[name='collaboratorEmail").value;
+
+                return {
+                    collaboratorName,
+                    collaboratorEmail
+                }
+            })
+
+        // event-ify data so it works with onChange
+
+        collaborators = collaborators.filter((item, i, arr) => {
+            return !(
+                item.name === "" && item.email === "" && i < (arr.length - 1)
+            )
         })
+
+        let collaboratorsEvent = {
+            target: {
+                name: "collaborators",
+                value: collaborators
+            },
+            preventDefault: () => {}
+        }
+
+        return onChange(collaboratorsEvent)
     }
 
     const collaborators = values.collaborators.map((collaborator, i) => {
@@ -34,6 +61,17 @@ export const Collaborators = ({
             key = { i + 2 }
         />
     })
+
+    return (
+        <section>
+            <h5>Step {stepNumber}: Recipient Details</h5>
+            <CollaboratorList
+                values = { values }
+                onChange = { onChange }
+            />
+            
+        </section>
+    )
 
     return (
         <section>
@@ -74,38 +112,5 @@ export const Collaborators = ({
         </section>
     )
 
-    // return (
-    //     <section>
-    //         <h5>Step {stepNumber}: Recipient Details</h5>
-            
-    //         <CollaboratorDetails
-    //             name = {{
-    //                 elementName: "recipientName",
-    //                 elementLabel: "Recipient's Name",
-    //                 value: values.recipientName
-    //             }}
-    //             email = {{
-    //                 elementName: "recipientEmail",
-    //                 elementLabel: "Recipient's Email",
-    //                 value: values.recipientEmail
-    //             }}
-    //             onChange = { onChange } />
-
-    //         <CollaboratorDetails
-    //             name = {{
-    //                 elementName: "recipientName",
-    //                 elementLabel: "Recipient's Name",
-    //                 value: values.recipientName
-    //             }}
-    //             email = {{
-    //                 elementName: "recipientEmail",
-    //                 elementLabel: "Recipient's Email",
-    //                 value: values.recipientEmail
-    //             }}
-    //             onChange = { onChange } />
-
-    //         { collaborators }
-
-    //     </section>
-    // )
+    
 }
