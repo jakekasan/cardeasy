71e722fc41baf23d4912085f2662b1d5073e95b4
jakekasan <jake.kasan@gmail.com>
2020-02-10 11:51:44 +0000

    Add SendDate component
    New component to specify when card will be sent.

---
+++ src/react/components/form/formParts/sendDate.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const SendDate = (props) => {
+    return (
+        <section>
+            <h5>Step {stepNumber}: Choose when to send card!</h5>
+            <div>
+                {  }
+                <input type="checkbox" name="" id=""/>
+            </div>
+        </section>
+    )
+}
