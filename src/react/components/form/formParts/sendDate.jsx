99892a30925d477202cceab2822e6c86573402b6
jakekasan <jake.kasan@gmail.com>
2020-02-10 12:15:35 +0000

    Update SendDate component
    Added helper function for initial form data.

--- src/react/components/form/formParts/sendDate.jsx
+++ src/react/components/form/formParts/sendDate.jsx
@@ -1,6 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
-export const SendDate = (props) => {
+function getDefaultData() {
+    const dateNow = new Date();
+    const dateToRemind = dateNow.setDate(dateNow.getDate + 1);
+    const dateToSend = dateNow.setDate(dateNow.getDate() + 2);
+
+    return {
+        dateToSendCard: dateToSend,
+        reminder: {
+            set: false,
+            when: dateToRemind
+        }
+    }
+}
+
+function useSendDateHelpers(sendDateData = getDefaultData()) {
+    const { dateToSendCard, reminder } = sendDateData;
+
+    const [ dateToSend, setDateToSend ] = useState(dateToSendCard);
+
+    return {
+        
+    }
+}
+
+export const SendDate = ({
+    onChange,
+    values,
+    stepNumber
+}) => {
     return (
         <section>
             <h5>Step {stepNumber}: Choose when to send card!</h5>
