a0979c2d56d66a03b53a6a597dd3940718df679c
jakekasan <jake.kasan@gmail.com>
2020-03-22 14:45:55 +0000

    Update SendDate component

--- src/react/components/form/formParts/sendDate.jsx
+++ src/react/components/form/formParts/sendDate.jsx
@@ -2,11 +2,11 @@ import React, { useState } from "react";
 
 function getDefaultData() {
     const dateNow = new Date();
-    const dateToRemind = dateNow.setDate(dateNow.getDate + 1);
+    const dateToRemind = dateNow.setDate(dateNow.getDate() + 1);
     const dateToSend = dateNow.setDate(dateNow.getDate() + 2);
 
     return {
-        dateToSendCard: dateToSend,
+        dateToSend: dateToSend,
         reminder: {
             set: false,
             when: dateToRemind
@@ -15,25 +15,33 @@ function getDefaultData() {
 }
 
 function useSendDateHelpers(sendDateData = getDefaultData()) {
-    const { dateToSendCard, reminder } = sendDateData;
+    const { dateToSend: dateToSendCard, reminder } = sendDateData;
 
     const [ dateToSend, setDateToSend ] = useState(dateToSendCard);
 
     function sendDateOnChange(event) {
-        const {
-            target: {
-                name,
-                value
-            }
-        } = event;
+        console.log("sendDateOnChange");
+        const { target } = event;
 
-        const formDate = Date.parse(value);
+        const { value } = target;
+
+        console.log(target);
+
+        console.log("Raw date is:", value);
+
+        const formDate = new Date(value);
+
+        console.log("Parsed date is:", formDate.toDateString());
 
         setDateToSend(formDate); // for now, later add validation
     }
 
-    return {
+    console.log("useSendDateHelpers");
+    console.log({dateToSend});
 
+    return {
+        sendDateOnChange,
+        dateToSend
     }
 }
 
@@ -42,12 +50,32 @@ export const SendDate = ({
     values,
     stepNumber
 }) => {
+    const { sendDateOnChange, dateToSend } = useSendDateHelpers();
+
+    console.log("SendDate render");
+    console.log(typeof dateToSend);
+
+    const date = new Date(dateToSend);
+
+    const year = date.getFullYear();
+    const month = ('0' + date.getMonth()).slice(-2);
+    const day = ('0' + date.getDate()).slice(-2);
+
+    const dateString = `${year}-${month}-${day}`;
+
+    console.log("Parsed string date = ", dateString);
     return (
         <section>
             <h5>Step {stepNumber}: Choose when to send card!</h5>
             <div>
                 {  }
-                <input type="checkbox" name="" id=""/>
+                <input
+                    type="date"
+                    name="dateToSendCard"
+                    onChange = { sendDateOnChange }
+                    //value = { dateString }
+                    value = { dateString }
+                    />
             </div>
         </section>
     )
