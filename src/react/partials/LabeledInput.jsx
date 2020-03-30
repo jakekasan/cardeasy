ac64ff8afd6db7d470b641b99121834445db06ad
jakekasan <jake.kasan@gmail.com>
2020-03-30 21:20:04 +0100

    Move LabeledInput
    Better location for helper components

---
+++ src/react/partials/LabeledInput.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+export const LabeledInput = ({
+    label,
+    name,
+    type,
+    onChange,
+    value
+}) => {
+    return (
+        <div>
+            <label
+                htmlFor = { name }
+            >
+                { label }
+            </label>
+            <input
+                name = { name }
+                type = { type }
+                value = { value }
+                onChange = { onChange }
+                />
+        </div>
+    )
+}
