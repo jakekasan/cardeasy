10e88ef9560e207caac1563da33813e9d6fee64b
jakekasan <jake.kasan@gmail.com>
2020-04-05 10:34:00 +0100

    Move CollaboratorList to new location

---
+++ src/react/components/form/setCollaborators/CollaboratorList.jsx
@@ -0,0 +1,90 @@
+import React from "react";
+import { LabeledInput } from "./labeledInput.jsx";
+
+function fixCollaborators(collabs) {
+    let fixedCollaborators = collabs.filter(item => {
+        return !(
+            item.collaboratorName === "" &&
+            item.collaboratorEmail === ""
+            )
+    })
+
+    if (fixedCollaborators.length === 0) {
+        return [{
+            collaboratorName: "",
+            collaboratorEmail: ""
+        }]
+    }
+
+    let lastCollaborator = fixedCollaborators[fixedCollaborators.length - 1];
+
+    if (lastCollaborator.collaboratorName !== "" || lastCollaborator.collaboratorEmail !== "") {
+        return [...fixedCollaborators, {
+            collaboratorName: "",
+            collaboratorEmail: ""
+        }]
+    }
+
+}
+
+export const CollaboratorList = ({
+    values,
+    onChange
+}) => {
+
+    const fixedCollaborators = fixCollaborators(values.collaborators);
+
+    function onCollabChangeFactory(index) {
+
+        function onCollabChange(event) {
+            
+            const newValues = fixedCollaborators;
+
+            newValues[index][event.target.name] = event.target.value;
+
+            return onChange({
+                preventDefault: () => {},
+                target: {
+                    name: "collaborators",
+                    value: newValues
+                }
+            })
+        }
+
+        return onCollabChange
+    }
+
+    function newOnChange(event) {
+        const { parentNode: { index }, target: { name, value }} = event;
+
+        values[index] = { ...values[index], [name]: value };
+
+        return onChange(values)
+    }
+
+    return (
+        <ul>
+            {fixedCollaborators.map((collab, index) => {
+                return (
+                    <li key = { index }>
+                        <LabeledInput
+                            label = { "Collaborator's Name" }
+                            name = { "collaboratorName" }
+                            type = { "text" }
+                            onChange = { onCollabChangeFactory(index) }
+                            value = { collab.collaboratorName }
+                            
+                        />
+                        <LabeledInput
+                            label = { "Collaborator's Email" }
+                            name = { "collaboratorEmail" }
+                            type = { "email" }
+                            onChange = { onCollabChangeFactory(index) }
+                            value = { collab.collaboratorEmail }
+                        />
+                    </li>
+                )
+            })}
+        </ul>
+    )
+}
