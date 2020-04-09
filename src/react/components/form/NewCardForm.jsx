f54c869f20b1b19e15d014b8cbac2999613978d5
jakekasan <jake.kasan@gmail.com>
2020-04-09 16:10:25 +0100

    Update NewCardForm to pass props to children

--- src/react/components/form/NewCardForm.jsx
+++ src/react/components/form/NewCardForm.jsx
@@ -3,14 +3,17 @@ import { MultiPage } from "../../partials/MultiPage.jsx";
 import { SetCardOccasion } from "./setCardOccasion/SetCardOccasion.jsx";
 import { SetCollaborators } from "./setCollaborators/SetCollaborators.jsx";
 import { SetDate } from "./setDate/SetDate.jsx";
+import { SetRecipientAndSender } from "./setRecipientAndSender/SetRecipientAndSender.jsx";
 
 export const NewCardForm = ({
-
+    ...props
 }) => {
     return (
-        <MultiPage>
+        <MultiPage { ...props }>
             <SetCardOccasion />
+            <SetRecipientAndSender />
             <SetCollaborators />
+            <SetMessage />
             <SetDate />
         </MultiPage>
     )
