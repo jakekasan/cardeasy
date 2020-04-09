6ea5afc622c4c6409152f2ddff1c6bfe9100cac2
jakekasan <jake.kasan@gmail.com>
2020-04-09 17:44:27 +0100

    Update NewCardForm to use card form hook

--- src/react/components/form/NewCardForm.jsx
+++ src/react/components/form/NewCardForm.jsx
@@ -4,12 +4,27 @@ import { SetCardOccasion } from "./setCardOccasion/SetCardOccasion.jsx";
 import { SetCollaborators } from "./setCollaborators/SetCollaborators.jsx";
 import { SetDate } from "./setDate/SetDate.jsx";
 import { SetRecipientAndSender } from "./setRecipientAndSender/SetRecipientAndSender.jsx";
+import { SetMessage } from "./setMessage/SetMessage.jsx";
+import { useCardFormValidation } from "./../../hooks/useCardFormValidation.jsx";
 
 export const NewCardForm = ({
     ...props
 }) => {
+
+    const { formData, updateFormData } = useCardFormValidation({
+        collaborators: [],
+        recipient: { name: "", email: ""},
+        sender: { name: "", email: "" },
+        message: "",
+        cardOccasion: "",
+        cardDesign: "",
+        sendDate: new Date()
+    });
+
+    const updatedProps = { ...props, values: formData, onChange: updateFormData };
+
     return (
-        <MultiPage { ...props }>
+        <MultiPage { ...updatedProps }>
             <SetCardOccasion />
             <SetRecipientAndSender />
             <SetCollaborators />
