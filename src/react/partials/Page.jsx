c6ba4feaad19b264fea5262df71e4e45e706d15b
jakekasan <jake.kasan@gmail.com>
2020-04-09 16:38:25 +0100

    Fix Page component argument destructuring

--- src/react/partials/Page.jsx
+++ src/react/partials/Page.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export const Page = ({content, title, partNumber, ...props}) => {
+export const Page = ({children:[content], title, partNumber, ...props}) => {
     return (
         <>
             <h5>Part {partNumber}: { title }</h5>
