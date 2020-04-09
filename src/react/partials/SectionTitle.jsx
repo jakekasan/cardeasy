f603bf749d446abbf0c51f00388641b396170716
jakekasan <jake.kasan@gmail.com>
2020-04-09 17:46:14 +0100

    Update SectionTitle to bring in props

--- src/react/partials/SectionTitle.jsx
+++ src/react/partials/SectionTitle.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 
-export const SectionTitle = () => {
+export const SectionTitle = ({
+    stepNumber,
+    title
+}) => {
     const titleText = `Step ${stepNumber}: ${title}`;
     return (
         <h5>{ titleText }</h5>
