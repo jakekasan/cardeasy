0e5be78cd42f26d54503c9bd2fd5150c63316158
jakekasan <jake.kasan@gmail.com>
2020-04-20 13:41:02 +0100

    Added empty onSubmit handler to useCardFormValidation

--- src/react/hooks/useCardFormValidation.jsx
+++ src/react/hooks/useCardFormValidation.jsx
@@ -2,15 +2,31 @@ import React, { useState } from "react";
 
 
 
-export const useCardFormValidation = (DEFAULT_FORM_VALUES) => {
-    let [ formData, setFormData ] = useState(DEFAULT_FORM_VALUES);
+export const useCardFormValidation = ({ defaultFormData = DEFAULT_FORM_VALUES}) => {
+    let [ formData, setFormData ] = useState(defaultFormData);
 
     const updateFormData = (newData) => {
         return setFormData({...formData, ...newData});
     }
+
+    const clearForm = () => {
+        setFormData(DEFAULT_FORM_VALUES);
+    }
+
+    const onSubmit = (event) => {
+        //event.preventDefault();
+
+        console.log(event);
+
+        console.log(formData);
+
+        //clearForm();
+    }
+
     return {
         updateFormData,
         formData,
-        setFormData
+        setFormData,
+        onSubmit
     }
 }
