c71d458ff8979611a101a82c9f757a2c87197adf
jakekasan <jake.kasan@gmail.com>
2020-04-20 13:42:08 +0100

    Update unused Page component
    Added logging before realizing that this element DOESN'T wrap pages.

--- src/react/partials/Page.jsx
+++ src/react/partials/Page.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 
 export const Page = ({children:[content], title, partNumber, ...props}) => {
+    console.log("Rendering page");
+    console.log(partNumber);
     return (
         <>
             <h5>Part {partNumber}: { title }</h5>
