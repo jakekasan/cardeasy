9227d3a0ed6357756d71434bdcbf491944ea3ff2
jakekasan <jake.kasan@gmail.com>
2020-04-23 08:48:49 +0100

    Update SetDate render
    Date and Time components now split and passed through to DateInputTemplate

--- src/react/components/form/setDate/SetDate.jsx
+++ src/react/components/form/setDate/SetDate.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { LabeledInput } from "../../../partials/LabeledInput.jsx";
 import { useDateForm } from "./../../../hooks/useDateForm.jsx";
 import { Page } from "./../../../partials/Page.jsx";
+import { DateInputTemplate } from "./DateInputTemplate.jsx";
 
 export const SetDate = ({
     onChange,
@@ -14,7 +15,7 @@ export const SetDate = ({
     const renderDatePart = (datePart, i) => {
         return (
             <LabeledInput
-                type = "number"
+                type = "text"
                 name = { datePart.name }
                 value = { datePart.value }
                 onChange = { datePart.onChange }
@@ -23,14 +24,26 @@ export const SetDate = ({
             />
         )
     }
+
+    const dateInputs = dateParts.filter(part => ["year", "month", "day"].includes(part.name)).map(part => renderDatePart(part, part.name));
+    const timeInputs = dateParts.filter(part => ["hour", "minute"].includes(part.name)).map(part => renderDatePart(part, part.name));
+
     return (
         <Page
             stepNumber = {stepNumber}
             title = "When would you like this card to be delivered?"
         >
-            {
+            
+            <DateInputTemplate
+                dateInputs={dateInputs}
+                timeInputs={timeInputs}
+            />
+
+            
+            
+            {/* {
                 dateParts.map((datePart, i) => renderDatePart(datePart, i))
-            }
+            } */}
         </Page>    
     )
 }
