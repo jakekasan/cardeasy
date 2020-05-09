9cf527451224fd2bf8fb8158d9fc80b25a18b0d8
jakekasan <jake.kasan@gmail.com>
2020-05-09 13:08:08 +0100

    Update Page component
    Added way to pass section and section styles in

--- src/react/partials/Page.jsx
+++ src/react/partials/Page.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import { SectionTitle } from "./SectionTitle.jsx";
 
-export const Page = ({children, title, stepNumber, ...props}) => {
+export const Page = ({children, title, stepNumber, styles = {}, ...props}) => {
     console.log("Rendering page");
     console.log(stepNumber);
+    const { sectionClassname, titleClassname } = styles;
     return (
         <>
             <SectionTitle
                 title={title}
                 stepNumber={stepNumber}
+                titleClassname={titleClassname}
             />
-            <section>
+            <section className={ sectionClassname }>
             { children }
             </section>
         </>
