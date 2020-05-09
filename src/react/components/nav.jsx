f9c013a3d27d542cecb91533f740e60f159c1498
jakekasan <jake.kasan@gmail.com>
2020-05-09 11:41:00 +0100

    Refactor Nav to functional component

--- src/react/components/nav.jsx
+++ src/react/components/nav.jsx
@@ -1,31 +1,23 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
-class Nav extends Component {
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        return (
-            <nav>
-                <ul>
-                    <li>
-                        <Link className="underlined" to = "/" >Home</Link>
-                    </li>
-                    <li>
-                        <Link className="underlined" to = "/new" >Start</Link>
-                    </li>
-                    <li>
-                        <Link className="underlined" to = "/sign" >Sign</Link>
-                    </li>
-                    <li>
-                        <Link className="underlined" to = "/about" >About</Link>
-                    </li>
-                </ul>
-            </nav>
-        )
-    }
+export const Nav = (props) => {
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <Link className="underlined" to = "/" >Home</Link>
+                </li>
+                <li>
+                    <Link className="underlined" to = "/new" >Start</Link>
+                </li>
+                <li>
+                    <Link className="underlined" to = "/sign" >Sign</Link>
+                </li>
+                <li>
+                    <Link className="underlined" to = "/about" >About</Link>
+                </li>
+            </ul>
+        </nav>
+    )
 }
-
-export default Nav;
