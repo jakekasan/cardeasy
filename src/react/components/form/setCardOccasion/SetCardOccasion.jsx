7fc5eefa40f4045df4c7273e2082b96c752fd324
jakekasan <jake.kasan@gmail.com>
2020-05-27 16:09:55 +0100

    Add 'selected' logic to SetCardOccasion

--- src/react/components/form/setCardOccasion/SetCardOccasion.jsx
+++ src/react/components/form/setCardOccasion/SetCardOccasion.jsx
@@ -1,22 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { CardOccasion } from "./CardOccasion.jsx";
 import { Page } from "./../../../partials/Page.jsx";
+
+const occasionNames = ["Birthday", "New Child", "Anniversary", "Bar Mitzvah"];
+
+const DEFAULT_OCCASIONS = occasionNames.map((name, i) => {
+    return {
+        name: name,
+        id: i,
+        selected: false
+    }
+})
 
 export const SetCardOccasion = ({
     onChange,
     values,
     stepNumber
 }) => {
+
+    const [ occasions, setOccasions ] = useState(DEFAULT_OCCASIONS);
+
+    function occasionOnClick(id) {
+        console.log("Clicked on occasion", id);
+        console.log("Is string", id instanceof String);
+
+        setOccasions(
+            occasions.map(item => {
+                item.selected = item.id === id;
+                return item
+            })
+        )
+    }
+
+    useEffect(() => {
+        console.log("SetCardOccasion starting");
+        return () => console.log("SetCardOccasion stopping")
+    })
+
     return (
         <Page
             title = "Choose an occasion for your card"
             stepNumber = { stepNumber }
         >
             <ul className = "CardOccasion">
-                <CardOccasion />
-                <CardOccasion />
-                <CardOccasion />
-                <CardOccasion />
+                { occasions.map((occasion, i) => <CardOccasion key={i} occasion={ occasion } occasionOnClick={ occasionOnClick }/>)}
             </ul>
         </Page>
     )
