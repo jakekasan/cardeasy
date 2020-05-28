de906074d28ae7f0b19a389ab142834ea774dc83
jakekasan <jake.kasan@gmail.com>
2020-05-28 16:07:53 +0100

    Remove unnecessary code

--- src/react/components/form/setCollaborators/SetCollaborators.jsx
+++ src/react/components/form/setCollaborators/SetCollaborators.jsx
@@ -8,41 +8,7 @@ export const SetCollaborators = ({
     stepNumber
 }) => {
 
-    function collaboratorOnChange(event){
-        let target = event.target;
-        let parentNode = ((target.parentNode).parentNode);
 
-        const collaborators = Array.from(parentNode.childNodes)
-            .filter(child => child.getAttribute("data-type") === "collaborator")
-            .map(child => {
-                let collaboratorName = child.querySelector("input[name='collaboratorName'").value;
-                
-                let collaboratorEmail = child.querySelector("input[name='collaboratorEmail").value;
-
-                return {
-                    collaboratorName,
-                    collaboratorEmail
-                }
-            })
-
-        // event-ify data so it works with onChange
-
-        collaborators = collaborators.filter((item, i, arr) => {
-            return !(
-                item.name === "" && item.email === "" && i < (arr.length - 1)
-            )
-        })
-
-        let collaboratorsEvent = {
-            target: {
-                name: "collaborators",
-                value: collaborators
-            },
-            preventDefault: () => {}
-        }
-
-        return onChange(collaboratorsEvent)
-    }
 
     return (
         <Page
