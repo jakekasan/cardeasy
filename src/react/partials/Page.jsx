28195abcc88ac845276718dbeef9624fbf643357
jakekasan <jake.kasan@gmail.com>
2020-05-28 16:10:51 +0100

    Add useEffect log to Page

--- src/react/partials/Page.jsx
+++ src/react/partials/Page.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { SectionTitle } from "./SectionTitle.jsx";
 
 export const Page = ({children, title, stepNumber, styles = {}, ...props}) => {
-    console.log("Rendering page");
-    console.log(stepNumber);
+    
+    useEffect(() => {
+        console.log(`Page number ${stepNumber} rendering`);
+        return () => {
+            console.log(`Page number ${stepNumber} unmounting`)
+        }
+    })
+
     const { sectionClassname, titleClassname } = styles;
+    
     return (
         <>
             <SectionTitle
