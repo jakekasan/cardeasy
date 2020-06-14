09dc6d249c4a760f0141d10f77325f4c74327a3a
jakekasan <jake.kasan@gmail.com>
2020-06-14 13:47:56 +0100

    Update Home
    Added temporary media query tests.

--- src/react/components/home.jsx
+++ src/react/components/home.jsx
@@ -1,13 +1,39 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
+
+function useMedia(query) {
+    const [ matches, setMatches ] = useState(window.matchMedia(query).matches);
+    
+    useEffect(() => {
+        let media = window.matchMedia(query);
+        if (media.matches != matches) {
+            setMatches(media);
+        };
+        let listener = () => setMatches(media.matches);
+        media.addListener(listener);
+
+        return () => media.removeListener(listener);
+    }, [query])
+
+    return matches
+}
+
 export const Home = () => {
+
+    let massive = useMedia("(max-width: 1200px");
+    let large = useMedia("(max-width: 800px");
+    let medium = useMedia("(max-width: 500px)");
+    let phone = useMedia("(max-width: 400px)")
+
+    console.log(massive, large, medium, phone);
+
     return (
         <main>
-            <ul>
-                <Link className="buttonStle" to="/new">Start</Link>
-                <Link className="buttonStle" to="/sign">Sign</Link>
-            </ul>
+            <p>Massive? {(massive) ? "Yes" : "Nope"}</p>
+            <p>Large? {(large) ? "Yes" : "Nope"}</p>
+            <p>Medium? {(medium) ? "Yes" : "Nope"}</p>
+            <p>Phone? {(phone) ? "Yes" : "Nope"}</p>
         </main>
     )
 };
