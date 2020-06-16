1e72d8beeb2ee8dbc3263af5aaf739ca803645c7
jakekasan <jake.kasan@gmail.com>
2020-06-16 15:03:57 +0100

    Update to use new DateInput and TimeInput

--- src/react/components/form/setDate/DateInputTemplate.jsx
+++ src/react/components/form/setDate/DateInputTemplate.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { TimeInput } from "./TimeInput.jsx";
+import { DateInput } from "./DateInput.jsx";
 
 export const DateInputTemplate = (props) => {
 
@@ -7,17 +9,21 @@ export const DateInputTemplate = (props) => {
     const dateMessage = "Enter the date that you'd like the card to be sent at:";
     const timeMessage = "And the time:"
 
+    let liWrap = element => <li>{ element }</li>
+
     return (
-        <>
-        
-        <p>{ dateMessage }</p>
+        <div className = "SetDate">
+            <p>{ dateMessage }</p>
+            {/* <ul className = "inputList" >{ dateInputs.map(liWrap) }</ul> */}
 
-        { dateInputs }
-
-        <p>{ timeMessage }</p>
-
-        { timeInputs }
-
-        </>
+            <DateInput dateInputs = { dateInputs} />
+            <br/>
+            <br/>
+            
+            <p>{ timeMessage }</p>
+            {/* <ul className = "inputList" >{ timeInputs.map(liWrap) }</ul> */}
+            
+            <TimeInput timeInputs = { timeInputs } />
+        </div>
     )
 }
