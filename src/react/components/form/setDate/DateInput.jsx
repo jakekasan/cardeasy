9d9ca567b63a23e10965e593addbc25305575c7c
jakekasan <jake.kasan@gmail.com>
2020-06-16 15:03:20 +0100

    Refactor DateInput
    Component now used to render year, month and day iputs

--- src/react/components/form/setDate/DateInput.jsx
+++ src/react/components/form/setDate/DateInput.jsx
@@ -2,23 +2,44 @@ import React from "react";
 import { useDateForm } from "../../../hooks/useDateForm.jsx";
 import { LabeledInput } from "../../../partials/LabeledInput.jsx";
 
-export default DateInput = () => {
+// export default DateInput = () => {
 
-    const { dateParts } = useDateForm();
+//     const { dateParts } = useDateForm();
 
-    const renderDatePart = (datePart) => {
-        return (
-            <LabeledInput
-                type = "number"
-                { ...datePart }
-            />
-        )
-    }
+//     const renderDatePart = (datePart) => {
+//         return (
+//             <LabeledInput
+//                 type = "number"
+//                 { ...datePart }
+//             />
+//         )
+//     }
+//     return (
+//         <section>
+//             {
+//                 dateParts.map(datePart => renderDatePart(datePart))
+//             }
+//         </section>    
+//     )
+// }
+
+const UnLabeledInput = (props) => <input className = "UnLabeledInput" type = "text" {...props} />
+
+export const DateInput = ({dateInputs: dateParts}) => {
+
+    const [ year ] = dateParts.filter(part => part.name === "year")
+    const [ month ] = dateParts.filter(part => part.name === "month")
+    const [ day ] = dateParts.filter(part => part.name === "day")
+
+    // return (
+    //     <div className = "inlineInputs">
+    //         <input className type="text" {...day}/> { "/" } <input type="text" {...month}/> { "/" } <input type="text" {...year}/> 
+    //     </div>
+    // )
+
     return (
-        <section>
-            {
-                dateParts.map(datePart => renderDatePart(datePart))
-            }
-        </section>    
+        <div className="inlineInputs">
+            <UnLabeledInput {...day} /> {"/"} <UnLabeledInput {...month} /> {"/"} <UnLabeledInput {...year} />
+        </div>
     )
 }
