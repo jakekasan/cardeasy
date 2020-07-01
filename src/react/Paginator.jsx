ed56d9063c4bbcbd675508a99fca820664b6d95a
jakekasan <jake.kasan@gmail.com>
2020-07-01 16:30:59 +0100

    Add pagination component classnames and fix bug
    Paginator was not stopping at maxPage, as it was calculated incorrectly

--- src/react/Paginator.jsx
+++ src/react/Paginator.jsx
@@ -1,22 +1,26 @@
 import React, { useState, useEffect, useReducer, createContext, useContext } from "react";
 
 const usePagination = ({ data, maxPerPage }) => {
-    console.log(`usePagination - setting maxPage = ${maxPage}`)
+    console.log(`usePagination - maxPerPage = ${maxPerPage}`)
 
-    let calcMaxPage = (data, maxPerPage) => Math.ceil(data.length, maxPerPage);
+    let calcMaxPage = (data, maxPerPage) => (data && data.length && data.length > 0) ? Math.ceil(data.length / maxPerPage) : 0;
 
+    console.log(`data.length = ${data.length}, data.length / maxPerPage should be ${data.length / maxPerPage}`)
     const [ canGoBack, setCanGoBack ] = useState(true);
     const [ canGoNext, setCanGoNext ] = useState(true);
     const [ maxPage, setMaxPage ] = useState(calcMaxPage(data, maxPerPage));
     const canGoPage = (page) => 0 >= page >= maxPage
 
     useEffect(() => {
+        console.log("Data or maxPerPage have now changed")
+        console.log(`maxPage was ${maxPage}`)
         setMaxPage(calcMaxPage(data, maxPerPage))
+        console.log(`maxPage is now ${maxPage}`)
     }, [ data, maxPerPage ])
 
     const reducer = (currentPage, action) => {
         console.log("Reducer in usePagination called");
-        console.log(`Current page: ${currentPage}`)
+        console.log(`Current page: ${currentPage}, maxPage: ${maxPage}`);
         console.log(action);
         let candidatePage;
 
@@ -24,7 +28,7 @@ const usePagination = ({ data, maxPerPage }) => {
             case "next":
                 if (!canGoNext) return currentPage
                 candidatePage = currentPage + 1;
-                return (currentPage < maxPage) ? candidatePage : currentPage
+                return (candidatePage < maxPage) ? candidatePage : currentPage
             case "back":
                 if (!canGoBack) return currentPage
                 candidatePage = currentPage - 1;
@@ -70,19 +74,19 @@ const PaginatorContext = createContext({});
 const BackButton = () => {
     const { goBackPage, canGoBack } = useContext(PaginatorContext);
 
-    return <button onClick={ goBackPage } { ...(canGoBack) ? null : { disabled: false }}>Back</button>
+    return <button className="BackButton" onClick={ goBackPage } { ...(canGoBack) ? null : { disabled: false }}>Back</button>
 }
 
 const NextButton = () => {
     const { goNextPage, canGoNext } = useContext(PaginatorContext);
 
-    return <button onClick={ goNextPage } { ...(canGoNext) ? null : { disabled: false }}>Next</button>
+    return <button className="NextButton" onClick={ goNextPage } { ...(canGoNext) ? null : { disabled: false }}>Next</button>
 }
 
 const Paginator = ({ data, maxPerPage, className, children } = { maxPerPage: 1, className: "Paginator", data: [] }) => {
 
     return (
-        <section className={ className }>
+        <section className="Paginator">
             <PaginatorContext.Provider value={ usePagination({ data, maxPerPage })}>
                 { children }
             </PaginatorContext.Provider>
