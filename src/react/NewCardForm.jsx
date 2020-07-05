1e9f5863545a9e3a620efb5a4d5dd29cc38559d9
jakekasan <jake.kasan@gmail.com>
2020-07-05 21:00:23 +0100

    Remove unused Form component and console.logs

--- src/react/NewCardForm.jsx
+++ src/react/NewCardForm.jsx
@@ -1,23 +1,7 @@
 import React, { useEffect, useReducer, Children, cloneElement, createContext, useState, useContext } from "react";
 
 import { Paginator, PaginatorContext, BackButton, NextButton } from "./Paginator.jsx";
-
-const MultiPartForm = ({ children }) => {
-    const [ isFormChildValid, setIsFormChildValid ] = useState(false);
-
-    const { currentPage, nextPage, prevPage } = usePagination({ maxPage: children.length - 1});
-
-    return (
-        <section className="MultiPartForm">
-            <article>
-                { React.cloneElement( children[state.currentPage], { pageNumber: currentPage + 1, setIsValid: setFromChildValid })}
-            </article>
-            <button className={ "BackButton" } onClick={ nextPage }>Back</button>
-            <button className={ "NextButton" } onClick={ prevPage }>{ (currentPage < children.length - 1) ? "Next" : "Submit" }</button>
-        </section>
-    )
-}
-
+import SetOccasion from "./SetOccasion.jsx";
 
 const SetSender = () => {
     return (
@@ -46,9 +30,7 @@ const JustRender = () => {
 }
 
 const NewCardForm = ({ children }) => {
-    console.log("NewCardForm rendering, children:");
     children = [<SetOccasion/>, <SetSender/>];
-    console.log(children);
     return (
         <Paginator data={ children } maxPerPage={ 1 }>
             <JustRender/>
