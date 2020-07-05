1a82de9180be8e4a34ba37fe7d01f2e2efef80fa
jakekasan <jake.kasan@gmail.com>
2020-07-05 18:39:21 +0100

    Refactor Nav, Header, Footer and Content to separate page

--- src/react/App.jsx
+++ src/react/App.jsx
@@ -4,68 +4,20 @@ import {
     BrowserRouter as Router,
     Route,
     Link,
-    Switch
+    Switch,
+    useLocation
 } from "react-router-dom";
 
 import NewCardForm from "./NewCardForm.jsx";
+import { Header, Footer, Content, Nav } from "./Layout.jsx";
 import "./Styles.sass";
 
-const MainLogo = () => <h1>CardEasy</h1>
-
-const Nav = () => {
-    return (
-        <nav>
-            <Link to="/new">New</Link>
-            <Link to="/sign">Sign</Link>
-            <Link to="/about">About</Link>
-        </nav>
-    )
-}
-
-const Header = () => {
-    return (
-        <header>
-            <MainLogo title={ "Cardeasy" } />
-            <Nav />
-        </header>
-    )
-}
-
-const Footer = () => {
-    return (
-    <footer>
-        <h3>End of the page</h3>
-    </footer>
-    )}
-
-const ChildOne = (props) => {
-    useEffect(() => () => console.log("ChildOne unmounting"), []);
-
-    return (
-        <>
-        <h5>First Child</h5>
-        <input type="text" onBlur={ () => console.log("input blurring") }/>
-        </>
-    )
-}
-
-const ChildTwo = (props) => {
-    useEffect(() => () => console.log("ChildTwo unmounting"), []);
-
-    return (
-        <>
-        <h5>Second Child</h5>
-        <div>
-            <input type="color"/>
-        </div>
-        </>
-    )
-}
-
-
 const PageNotFound = () => <h3>Oops...</h3>
-const SplashPage = () => <h3>Blamo! You're home!</h3>
-const Content = ({children}) => <main>{ children }</main>
+const SplashPage = () => {
+    return (
+        <Nav />
+    )
+}
 
 const App = () => {
     return (
@@ -78,10 +30,6 @@ const App = () => {
                             <SplashPage />
                         </Route>
                         <Route path="/new">
-                            {/* <MultiPartForm>
-                                <ChildOne />
-                                <ChildTwo />
-                            </MultiPartForm> */}
                             <NewCardForm />
                         </Route>
                         <Route path="*">
@@ -96,4 +44,3 @@ const App = () => {
 }
 
 export default App;
-// ReactDOM.render(<NewApp />, document.querySelector("#App"));
---
+++ src/react/Layout.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import {
+    BrowserRouter as Router,
+    Link,
+    useLocation
+} from "react-router-dom";
+
+const Nav = () => {
+    return (
+        <nav>
+            <Link to="/new">New</Link>
+            <Link to="/sign">Sign</Link>
+            <Link to="/about">About</Link>
+        </nav>
+    )
+}
+
+const MainLogo = () => <h1>CardEasy</h1>
+
+const Header = () => {
+    const location = useLocation();
+
+    return (
+        <header>
+            <MainLogo title={ "Cardeasy" } />
+            { (location.pathname == "/" ) ? undefined : <Nav /> }
+        </header>
+    )
+}
+
+const Footer = () => {
+    return (
+        <footer>
+            <h3>End of the page</h3>
+        </footer>
+    )}
+
+const Content = ({children}) => <main>{ children }</main>
+
+export {
+    Header,
+    Footer,
+    Content,
+    Nav
+}
