e565d5c24d57fe04b12a2d75b0bd51ba4b0454b6
jakekasan <jake.kasan@gmail.com>
2020-07-28 22:22:22 +0100

    Refactor out pagination Blocks

--- src/react/SetOccasion.jsx
+++ src/react/SetOccasion.jsx
@@ -4,11 +4,12 @@ import React, {
     useState
 } from "react";
 
-import { Paginator, BackButton, NextButton, PaginatorContext, BackBlade, NextBlade } from "./Paginator.jsx";
+import { Paginator, BackButton, NextButton, PaginatorContext, BackBlade, NextBlade, Slider } from "./Paginator.jsx";
 import { Title, Page, Content } from "./Layout.jsx";
+import { Block, BlockList } from "./BlockList";
 
 
-const occasionNames = ["Birthday", "New Child", "Anniversary", "Bar Mitzvah", "Funeral", "Wedding", "High School Graduation", "University Graduation", "Becoming a Grandparent", "Bereavement", "New Star Wars Movie", "Won Lottery"];
+const occasionNames = ["Birthday", "New Child", "Anniversary", "Bar Mitzvah", "Funeral", "Wedding", "High School Graduation"] // "University Graduation", "Becoming a Grandparent", "Bereavement", "New Star Wars Movie", "Won Lottery"];
 
 const DEFAULT_OCCASIONS = occasionNames.map((name, i) => {
     return {
@@ -17,30 +18,6 @@ const DEFAULT_OCCASIONS = occasionNames.map((name, i) => {
         selected: false
     }
 })
-
-const DEFAULT_OCCASION = { name: "Unknown Occasion" };
-
-export const CardOccasion = ({ occasion = DEFAULT_OCCASION, occasionOnClick: onClick}) => {
-
-    const selected = (occasion.selected) ? " selected" : "";
-    return (
-        <li className={`CardOccasion${selected}`} id={ occasion.id } onClick={ () => onClick(occasion.id) }>
-            <p>{ occasion.name }</p>
-        </li>
-    )
-}
-
-const Occasion = ({ name }) => <li className="Occasion">{ name }</li>
-
-const OccasionList = () => {
-    const { currentView } = useContext(PaginatorContext);
-
-    return (
-        <ul className="content OccasionList">
-            { currentView.map(occasion => <Occasion key={occasion.id} {...occasion} />)}
-        </ul>
-    )
-}
 
 const SetOccasion = () => {
 
@@ -52,8 +29,8 @@ const SetOccasion = () => {
         setCanGoNext(() => false);
         console.log("canGoNext set to false")
         let timeOuts = [
-            setTimeout(() => setOccasions(DEFAULT_OCCASIONS), 3000),
-            setTimeout(() => setCanGoNext(true), 5000)
+            setTimeout(() => setOccasions(DEFAULT_OCCASIONS), 1000),
+            setTimeout(() => setCanGoNext(true), 4000)
         ];
         
         return () => timeOuts.forEach(clearTimeout);
@@ -64,9 +41,8 @@ const SetOccasion = () => {
             <Title>{ currentPage + 1 }: Choose an occasion for this card!</Title>
             <Content>
                 <Paginator data={ occasions } maxPerPage={ 10 }>
-                    <OccasionList />
-                    <BackBlade />
-                    <NextBlade />
+                    <BlockList />
+                    <Slider />
                 </Paginator>
             </Content>
         </>
