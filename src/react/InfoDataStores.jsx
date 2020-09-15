ae7344a69adcecf77649eeb5f29452ca6828ca05
jakekasan <jake.kasan@gmail.com>
2020-09-15 10:48:44 +0200

    Add InfoDataStore
    In the future this will pull occasion and design information from API

---
+++ src/react/InfoDataStores.jsx
@@ -0,0 +1,66 @@
+import { get } from "mongoose";
+import React from "react";
+
+import { DEFAULT_DESIGNS, DEFAULT_OCCASIONS } from "./Data";
+
+export const DataStoreContext = React.createContext({});
+
+const useDataStore = () => {
+
+    const occasions = {
+        getAll: () => {
+            return new Promise((res, rej) => {
+                return res(DEFAULT_OCCASIONS);
+            })
+        },
+        getById: (id) => {
+            return new Promise((res, rej) => {
+                try {
+                    const [ result ] = DEFAULT_OCCASIONS.filter(item => item.id === id);
+                    return res(result)
+                } catch {
+                    return rej({
+                        message: "No such item found."
+                    })
+                }
+            })
+        }
+    }
+
+    const designs = {
+        getAll: () => {
+            return new Promise((res, rej) => {
+                return res(DEFAULT_DESIGNS);
+            })
+        },
+        getById: (id) => {
+            return new Promise((res, rej) => {
+                try {
+                    const [ result ] = DEFAULT_DESIGNS.filter(item => item.id === id);
+                    return res(result)
+                } catch {
+                    return rej({
+                        message: "No such item found."
+                    })
+                }
+            })
+        }
+    }
+
+    return {
+        occasions,
+        designs
+    }
+}
+
+const InfoDataStore = ({ children }) => {
+    const { designs, occasions } = useDataStore();
+    
+    return (
+        <DataStoreContext.Provider value={ { designs, occasions } }>
+            { children }
+        </DataStoreContext.Provider>
+    )
+}
+
+export default InfoDataStore;
