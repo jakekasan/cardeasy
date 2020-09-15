40dad10e8b323d30f11ff931d096f94732f2d241
jakekasan <jake.kasan@gmail.com>
2020-09-15 10:47:46 +0200

    Update SetOccasion to get occasion data from store

--- src/react/SetOccasion.jsx
+++ src/react/SetOccasion.jsx
@@ -3,11 +3,19 @@ import React, {useContext, useState, useEffect} from "react";
 import { StoreContext } from "./FormDataStore";
 import { PaginationContext } from "./Pagination";
 import SelectableList from "./SelectableList";
+import { DataStoreContext} from "./InfoDataStores";
 
 const SetOccasion = () => {
     const { get, set } = useContext(StoreContext);
     const { goNext, currentPage } = useContext(PaginationContext);
-    const occasions = ["Birthday", "Anniversary", "Moved house", "Graduated", "Christmas"].map((item, i) => { return {id: i, text: item}})
+    const { occasions: { getAll }} = useContext(DataStoreContext);
+    const [occasions, setOccasions] = useState([]);
+    // const occasions = ["Birthday", "Anniversary", "Moved house", "Graduated", "Christmas"].map((item, i) => { return {id: i, text: item}})
+
+    useEffect(() => {
+        getAll()
+            .then(data => setOccasions(data))
+    }, []);
 
     const title = `Step ${currentPage + 1}: Select an occasion:`
     return (
