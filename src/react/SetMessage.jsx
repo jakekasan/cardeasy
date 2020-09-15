316cef42b4ab64869faefe58f930338ffd531eee
jakekasan <jake.kasan@gmail.com>
2020-09-15 10:47:09 +0200

    Update SetMessage title
    Consumes PaginationContext for step number

--- src/react/SetMessage.jsx
+++ src/react/SetMessage.jsx
@@ -1,18 +1,24 @@
-import React from "react";
+import React, { useContext } from "react";
 import styled from "styled-components";
 
 import { TitleElement, TitledContent } from "./Layout";
+import { PaginationContext } from "./Pagination";
 
 const Textarea = styled.textarea`
     resize: false;
-    padding: 10px;
+    padding: 20px;
+    height: 50vh;
+    margin: 10px 100px;
+    font-size: 1.5rem;
 `;
 
 const SetMessage = () => {
+    const { currentPage } = useContext(PaginationContext);
+    const title = `Step ${currentPage}: What would you like the card to say?`;
     return (
         <TitledContent>
-            <TitleElement>Enter a message:</TitleElement>
-            <Textarea name="message" id="message" cols="30" rows="10" notresizable></Textarea>   
+            <TitleElement>{ title }</TitleElement>
+            <Textarea name="message" id="message" notresizable></Textarea>   
         </TitledContent>
     )
 }
