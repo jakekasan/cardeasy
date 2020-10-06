4ac616962d847a703b23c2a31693a73b675b49b2
jakekasan <jake.kasan@gmail.com>
2020-10-06 23:13:04 +0200

    Add individual repos to API route

--- back-end/routes/api.ts
+++ back-end/routes/api.ts
@@ -1,5 +1,10 @@
 import { NextFunction, Request, Response, Router } from "express";
+import { designRepo, occasionRepo, userRepo } from "../repos";
+import { IOccasion } from "../repos/OccasionRepository";
+import { IDesign } from "../repos/DesignRepository";
+import { IUser } from "../repos/UserRepository";
 import Card from "./card";
+import repoRouterFactory from "./repoRouterFactory";
 
 const api = Router();
 
@@ -9,6 +14,10 @@ api.use((req: Request, res: Response, next: NextFunction) => {
 });
 
 api.use("/card", Card);
+
+api.use("/occasion", repoRouterFactory<IOccasion>(occasionRepo));
+api.use("/design", repoRouterFactory<IDesign>(designRepo));
+api.use("/user", repoRouterFactory<IUser>(userRepo));
 
 api.use((req: Request, res: Response, next: NextFunction) => {
     console.log("API didn't match...");
