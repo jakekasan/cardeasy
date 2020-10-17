3762833f810348997af3ba5fccd361a99589f610
jakekasan <jake.kasan@gmail.com>
2020-10-17 20:47:11 +0200

    Change exports to actual DataStores

--- front-end/src/react/utils/Store.jsx
+++ front-end/src/react/utils/Store.jsx
@@ -30,14 +30,49 @@ const FakeDataStoreFactory = (data, storeName) => {
             }, 1000)
         })
     }
-
     return { getById }
 }
 
-const UserStore = FakeDataStoreFactory(FAKE_USERS, "User");
-const CardStore = FakeDataStoreFactory(FAKE_CARDS, "Card");
-const OccasionStore = FakeDataStoreFactory(FAKE_OCCASIONS, "Occasion");
-const DesignStore = FakeDataStoreFactory(FAKE_DESIGNS, "Design");
+const RealDataStoreFactory = ({ storeName, storeEndpoint }) => {
+
+    const getById = (id, session) => {
+        console.log(`${storeName} Store: Received query for id == ${id}, with session = ${session}`);
+
+        if (id === null || session === null) {
+            return new Promise((resolve, reject) => {
+                reject({ message: `${storeName} Store: Invalid arguments: id='${id}', session='${session}'`})
+            })
+        }
+
+        return fetch(`${storeEndpoint}?id=${id}&session=${session}`)
+            .then(data => {
+                let parsedData = data.json();
+                if (parsedData === {}) {
+                    return {
+                        message: `Could not find by id: ${id}`
+                    }
+                } else {
+                    return parsedData
+                }
+            })
+            .catch(err => {
+                return {
+                    message: err
+                }
+            })
+    }
+    return { getById }
+}
+
+// const UserStore = FakeDataStoreFactory(FAKE_USERS, "User");
+// const CardStore = FakeDataStoreFactory(FAKE_CARDS, "Card");
+// const OccasionStore = FakeDataStoreFactory(FAKE_OCCASIONS, "Occasion");
+// const DesignStore = FakeDataStoreFactory(FAKE_DESIGNS, "Design");
+
+const UserStore = RealDataStoreFactory({ storeName: "User", storeEndpoint: "/api/user" });
+const CardStore = RealDataStoreFactory({ storeName: "Card", storeEndpoint: "/api/card" });
+const OccasionStore = RealDataStoreFactory({ storeName: "Occasion", storeEndpoint: "/api/occasion" });
+const DesignStore = RealDataStoreFactory({ storeName: "Design", storeEndpoint: "/api/design" });
 
 export {
     UserStore,
