4ff89f9b5c1ef75bd4ca1b25c93d33f0384fc15d
jakekasan <jake.kasan@gmail.com>
2020-10-17 20:36:11 +0200

    Switch to Design datastore

--- front-end/src/react/NewCard/SetDesign.jsx
+++ front-end/src/react/NewCard/SetDesign.jsx
@@ -1,25 +1,31 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import {StoreContext} from "./FormDataStore";
+import { DataStoreContext } from "./InfoDataStores";
 import {PaginationContext} from "./Pagination";
 import SelectableList from "./SelectableList";
 
-const DEFAULT_DESIGNS = [
-    "CLASSIC",
-    "VINTAGE",
-    "FUTURISTIC",
-    "BLUE",
-    "RED",
-    "PURPLE",
-    "PINK",
-    "TURQUOISE"
-]
+// const DEFAULT_DESIGNS = [
+//     "CLASSIC",
+//     "VINTAGE",
+//     "FUTURISTIC",
+//     "BLUE",
+//     "RED",
+//     "PURPLE",
+//     "PINK",
+//     "TURQUOISE"
+// ]
 
 const SetDesign = () => {
     const { get, set } = useContext(StoreContext);
+    const { designs: getAll } = useContext(DataStoreContext);
     const { goNext, currentPage } = useContext(PaginationContext);
-    const designs = DEFAULT_DESIGNS.map((item, i) => { return {id: i, text: item}})
+    //const designs = DEFAULT_DESIGNS.map((item, i) => { return {id: i, text: item}})
+    const [designs, setDesigns] = useState([]);
 
-    useEffect(() => console.log({currentPage}));
+    useEffect(() => {
+        getAll()
+            .then(data => setDesigns(data))
+    });
 
     const title = `Step ${currentPage + 1}: Select a design:`
     return (
