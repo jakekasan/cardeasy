37c3534a6a63fc31660bfac5259410537aecaad3
jakekasan <jake.kasan@gmail.com>
2020-10-17 23:29:18 +0200

    Comment out fake data store code for eslint

--- front-end/src/react/utils/Store.jsx
+++ front-end/src/react/utils/Store.jsx
@@ -1,37 +1,37 @@
-import {
-    FAKE_USERS,
-    FAKE_CARDS,
-    FAKE_OCCASIONS,
-    FAKE_DESIGNS
-} from "./fakeData";
+// import {
+//     FAKE_USERS,
+//     FAKE_CARDS,
+//     FAKE_OCCASIONS,
+//     FAKE_DESIGNS
+// } from "./fakeData";
 
 
-const FakeDataStoreFactory = (data, storeName) => {
-    const getById = (id, session) => {
-        console.log(`${storeName} Store: Received query for id == ${id}, with session = ${session}`);
+// const FakeDataStoreFactory = (data, storeName) => {
+//     const getById = (id, session) => {
+//         console.log(`${storeName} Store: Received query for id == ${id}, with session = ${session}`);
 
-        if (id === null || session === null) {
-            return new Promise((resolve, reject) => {
-                reject({ message: `${storeName} Store: Invalid arguments: id='${id}', session='${session}'`})
-            })
-        }
+//         if (id === null || session === null) {
+//             return new Promise((resolve, reject) => {
+//                 reject({ message: `${storeName} Store: Invalid arguments: id='${id}', session='${session}'`})
+//             })
+//         }
 
-        let query = data.find(item => item.id.toString() === id.toString());
-        return new Promise((resolve, reject) => {
-            setTimeout(() => {
-                console.log(`${storeName} Store starting timeout`);
-                if (query !== undefined) {
-                    return resolve(query)
-                } else {
-                    return reject({
-                        message: `Could not find by id: ${id}`
-                    })
-                }
-            }, 1000)
-        })
-    }
-    return { getById }
-}
+//         let query = data.find(item => item.id.toString() === id.toString());
+//         return new Promise((resolve, reject) => {
+//             setTimeout(() => {
+//                 console.log(`${storeName} Store starting timeout`);
+//                 if (query !== undefined) {
+//                     return resolve(query)
+//                 } else {
+//                     return reject({
+//                         message: `Could not find by id: ${id}`
+//                     })
+//                 }
+//             }, 1000)
+//         })
+//     }
+//     return { getById }
+// }
 
 const RealDataStoreFactory = ({ storeName, storeEndpoint }) => {
 
